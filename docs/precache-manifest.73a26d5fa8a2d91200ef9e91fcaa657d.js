eaa1f83b867e516f9629df678047367e33941489
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-05-17 15:14:49 +0200

    Build to docs for GitHub Pages

---
+++ docs/precache-manifest.73a26d5fa8a2d91200ef9e91fcaa657d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a7b8fe277f37ae3e5064ed5386bb89",
+    "url": "/tsx-xaml-render-props/index.html"
+  },
+  {
+    "revision": "e6fc68f31dd6e51962f8",
+    "url": "/tsx-xaml-render-props/static/css/main.1e1d582e.chunk.css"
+  },
+  {
+    "revision": "29bcdd7d13475dbb346b",
+    "url": "/tsx-xaml-render-props/static/js/2.f5a82c0a.chunk.js"
+  },
+  {
+    "revision": "e6fc68f31dd6e51962f8",
+    "url": "/tsx-xaml-render-props/static/js/main.dc17c39e.chunk.js"
+  },
+  {
+    "revision": "6bc63cf3677816c62db8",
+    "url": "/tsx-xaml-render-props/static/js/runtime~main.99a66bbd.js"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tsx-xaml-render-props/precache-manifest.73a26d5fa8a2d91200ef9e91fcaa657d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tsx-xaml-render-props/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
